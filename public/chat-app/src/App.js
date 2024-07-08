35e0117ea23350d73c333654915709595eb0f684
mihir-n25 <mihirnebani@gmail.com>
2024-07-08 15:33:21 +0530

    frontend

--- public/chat-app/src/App.js
+++ public/chat-app/src/App.js
@@ -1,13 +1,18 @@
-
-
-function App() {
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SetAvatar from "./components/SetAvatar";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+export default function App() {
   return (
-    <>
-    <div>
-      hi
-    </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/setAvatar" element={<SetAvatar />} />
+        <Route path="/" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
